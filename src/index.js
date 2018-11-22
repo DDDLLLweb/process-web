977b3590b3e144668d1a62cbfc8214ee3226e1a3
L1116 <976130720@qq.com>
2018-11-22 11:38:16 +0800

    框架搭建初步结合

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import {AppContainer} from 'react-hot-loader';
+import {Provider} from 'react-redux';
+import AppRouter from './router/router';
+import store from './redux/store';
+renderWithHotReload(AppRouter());
+function renderWithHotReload(RootElement) {
+    ReactDOM.render(
+            <Provider store={store}>
+                {RootElement}
+            </Provider>,
+        document.getElementById('root')
+    )
+ }
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
